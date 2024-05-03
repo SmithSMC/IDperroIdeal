d5edc8354a1b777786607c9f79a1e8b4159e6d49
SmithSMC <ssmolanoc@academia.usbbog.edu.co>
2024-05-03 16:10:34 -0500

    Se agrega logica

--- InicioSesion/inicioSesion.py
+++ InicioSesion/inicioSesion.py
@@ -1,67 +1,145 @@
 from kivy.app import App
+from kivy.uix.relativelayout import RelativeLayout
 from kivy.uix.label import Label
+from kivy.core.window import Window
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.core.window import Window
-from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
 
-class MainScreen(Screen):
-    def __init__(self, **kwargs):
-        super(MainScreen, self).__init__(**kwargs)
-        
-        layout = FloatLayout()
-        
-        # Agregar imagen de fondo
-        background = Image(source='fondo4.png', allow_stretch=True, keep_ratio=False)
-        layout.add_widget(background)
-        
-        # Label de bienvenida centrado
-        welcome_label = Label(text="¡Bienvenido de nuevo!", color=(168/255, 110/255, 69/255, 1), size_hint=(None, None), size=(330, 69), pos_hint={'center_x': 0.5, 'center_y': 0.8}, font_size='30sp', font_name='Roboto-Bold', halign='center', valign='middle')
-        layout.add_widget(welcome_label)
-        
-        # Layout para inputs y botones
-        input_layout = GridLayout(cols=1, size_hint=(None, None), size=(330, 200), pos_hint={'center_x': 0.5, 'center_y': 0.5})
-        
-        # Inputs de correo y contraseña
-        email_input = TextInput(hint_text="Correo", multiline=False, background_color=(241/255, 194/255, 163/255, 1), foreground_color=(0, 0, 0, 1), border=(1, 1, 1, 1), size_hint=(None, None), size=(330, 40))
-        password_input = TextInput(hint_text="Contraseña", multiline=False, password=True, background_color=(241/255, 194/255, 163/255, 1), foreground_color=(0, 0, 0, 1), border=(1, 1, 1, 1), size_hint=(None, None), size=(330, 40))
-        input_layout.add_widget(email_input)
-        input_layout.add_widget(password_input)
-        
-        # Botón transparente para "¿Olvidó su contraseña?"
-        forgot_password_button = Button(text="[color=(168/255, 110/255, 69/255, 1)]¿Olvidó su contraseña?[/color]", markup=True, background_normal='', background_color=(0, 0, 0, 0), size_hint_y=None, height=40)
-        forgot_password_button.bind(on_press=self.forgot_password_click)
-        input_layout.add_widget(forgot_password_button)
-        
-        # Botón de iniciar sesión
-        login_button = Button(text="Iniciar sesión", background_color=(168/255, 110/255, 69/255, 1), color=(1, 1, 1, 1), background_down='#F1C2A3', size_hint=(None, None), size=(330, 40))
-        input_layout.add_widget(login_button)
-        
-        # Texto de "¿Eres nuevo? Regístrate aquí"
-        register_label = Label(text="[color=(168/255, 110/255, 69/255, 1)]¿Eres nuevo?[/color] [u][color=(168/255, 110/255, 69/255, 1)]Regístrate aquí[/color][/u]", markup=True, size_hint_y=None, height=40)
-        input_layout.add_widget(register_label)
-        
-        layout.add_widget(input_layout)
-        
-        self.add_widget(layout)
-    
-    def forgot_password_click(self, instance):
-        instance.text = "[color=0000FF]¿Olvidó su contraseña?[/color]"  # Cambiar el color del texto a azul al hacer clic
+class CasillaTexto(TextInput):
+    pass
 
-class MyApp(App):
+class EnlaceLabel(Label):
+    pass
+
+class EnlaceIniciaSesionLabel(Label):
+    pass
+
+class MainApp(App):
     def build(self):
-        sm = ScreenManager()
-        main_screen = MainScreen(name='main')
-        sm.add_widget(main_screen)
-        return sm
+        self.layout = RelativeLayout()
 
-    def on_start(self):
+        Window.clearcolor = (1, 1, 1, 1)
         Window.size = (369, 640)
-        Window.clearcolor = (1, 1, 1, 1)  # Fondo blanco
-        self.title = 'Ingreso'  # Cambiar el nombre de la ventana
+
+        background = Image(source='fondo4.png')
+        self.layout.add_widget(background)
+
+        welcome_label = Label(
+            text="¡Bienvenido de nuevo!",
+            color=(168/255, 110/255, 69/255, 1),
+            size_hint=(None, None),
+            size=(330, 60),
+            pos_hint={'center_x': 0.5, 'center_y': 0.8},
+            font_size='30sp',
+            font_name='Inter.ttc',
+            halign='center',
+            valign='middle'
+        )
+        self.layout.add_widget(welcome_label)
+
+        email_input = CasillaTexto(
+            hint_text="Correo",
+            multiline=False,
+            background_color=(241/255, 194/255, 163/255, 1),
+            foreground_color=(0, 0, 0, 1),
+            border=(0, 0, 0, 0),
+            background_active='',  # Eliminar sombreado al hacer clic
+            size_hint=(None, None),
+            size=(330, 40),
+            pos_hint={'center_x': 0.5, 'center_y': 0.6},
+            font_name='Inter.ttc'
+        )
+        password_input = CasillaTexto(
+            hint_text="Contraseña",
+            multiline=False,
+            password=True,
+            background_color=(241/255, 194/255, 163/255, 1),
+            foreground_color=(0, 0, 0, 1),
+            border=(0, 0, 0, 0),
+            background_active='',  # Eliminar sombreado al hacer clic
+            size_hint=(None, None),
+            size=(330, 40),
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            font_name='Inter.ttc'
+        )
+        self.layout.add_widget(email_input)
+        self.layout.add_widget(password_input)
+
+        login_button = Button(
+            text="Iniciar sesión",
+            background_color=(168/255, 110/255, 69/255, 1),
+            color=(1, 1, 1, 1),
+            background_down='#A86E45',
+            size_hint=(None, None),
+            size=(330, 40),
+            pos_hint={'center_x': 0.5, 'center_y': 0.4},
+            font_name='Inter.ttc'
+        )
+        login_button.bind(on_press=self.verificar_contraseña)
+        self.layout.add_widget(login_button)
+
+        register_layout = BoxLayout(
+            orientation='horizontal',
+            size_hint=(None, None),
+            size=(330, 40),
+            pos_hint={'center_x': 0.55, 'center_y': 0.3}
+        )
+
+        register_label = Label(
+            text="¿Eres nuevo? ",
+            color=(0, 0, 0, 1),
+            size_hint=(None, None),
+            height=40,
+            font_name='Inter.ttc'
+        )
+        register_layout.add_widget(register_label)
+
+        register_button = Button(
+            text="Regístrate aquí",
+            background_normal='',
+            background_color=(1, 1, 1, 0),
+            color=(168/255, 110/255, 69/255, 1),
+            size_hint=(None, None),
+            height=40,
+            pos_hint={'center_x': 0.55, 'center_y': 0.5},
+            font_name='Inter.ttc',
+            background_down='',
+            bold=True
+        )
+        register_button.bind(on_press=self.ir_a_registro)
+        register_layout.add_widget(register_button)
+
+        self.layout.add_widget(register_layout)
+
+        return self.layout
+
+    def verificar_contraseña(self, instance):
+        correo = self.layout.children[2].text  # Acceder al TextInput del correo
+        contraseña = self.layout.children[3].text  # Acceder al TextInput de la contraseña
+
+        # Lógica de verificación de la contraseña
+        if contraseña == "contraseña_correcta":
+            print("Contraseña correcta. Iniciando sesión...")
+            # Aquí puedes agregar la lógica para iniciar sesión
+        else:
+            # Si la contraseña es incorrecta, mostrar un mensaje de error en color rojo
+            error_label = Label(
+                text="Contraseña incorrecta. Intentelo de nuevo",
+                color=(1, 0, 0, 1),  # Rojo
+                size_hint=(None, None),
+                size=(330, 30),
+                pos_hint={'center_x': 0.5, 'center_y': 0.3},
+                font_name='Inter.ttc'
+            )
+            self.layout.add_widget(error_label)
+
+    def ir_a_registro(self, instance):
+        print("Ir a la pantalla de registro")
+
+    def ir_a_inicio(self, instance):
+        print("Ir a la pantalla de inicio")
 
 if __name__ == '__main__':
-    MyApp().run()
+    MainApp().run()
